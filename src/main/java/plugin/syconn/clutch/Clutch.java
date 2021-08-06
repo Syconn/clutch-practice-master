35e2eb20d8f11a72652a05bac78814202694caec
Syconn <49461127+Syconn@users.noreply.github.com>
2021-08-06 17:21:29 -0400

    Initial commit

---
+++ src/main/java/plugin/syconn/clutch/Clutch.java
@@ -0,0 +1,17 @@
+package plugin.syconn.clutch;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Clutch extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
